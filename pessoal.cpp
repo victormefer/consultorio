d02a7b7382daf6f8bc40027870078c63b765deb2
rpenna <rigpenna@yahoo.com.br>
2012-12-06 13:18:44 -0800

    Create pessoal.cpp
    implementação das interaces definidas em pessoal.h

---
+++ pessoal.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <string>
+
+#include "pessoal.h"
+
+using namespace std;
+
+//Implementação dos construtores
+
+Pessoa::Pessoa(string name) {
+    nome = name;
+}
+
+Medico::Medico(string esp) {
+    especialidade = esp;
+}
+
+Paciente::Paciente(int age, string adress, string phone) {
+    idade = age;
+    endereco = adress;
+    telefone = phone;
+}
+
+//Implementação dos métodos da classe Pessoa
+string Pessoa::getName() {
+    return nome;
+}
+
+//Implementação dos métodos da classe Médico
+string Medico::getEsp() {
+    return especialidade;
+}
+
+//Implementação dos métodos de Paciente
+int Paciente::getAge(){
+    return idade;
+}
+
+string Paciente::getAdress() {
+    return endereco;
+}
+
+string Paciente::getPhone() {
+    return telefone;
+}
