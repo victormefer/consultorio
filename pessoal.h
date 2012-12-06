a99016c860be85316b39ea3d35e67d43e5ad1b4c
rpenna <rigpenna@yahoo.com.br>
2012-12-06 13:16:10 -0800

    pessoal.h
    Definição de interfaces.

---
+++ pessoal.h
@@ -0,0 +1,35 @@
+#ifndef PESSOAL_H
+#define  PESSOAL_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Pessoa
+{
+    string nome;
+public:
+    Pessoa(string name);
+    string getName();
+};
+
+class Medico : public Pessoa {
+    string especialidade;
+public:
+    Medico(string esp);
+    string getEsp();
+};
+
+class Paciente : public Pessoa {
+    int idade;
+    string endereco;
+    string telefone;
+public:
+    Paciente(int age, string adress, string phone);
+    int getAge();
+    string getAdress();
+    string getPhone();
+};
+
+#endif	/* PESSOAL_H */
